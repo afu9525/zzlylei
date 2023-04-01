77a1d00c92bb0bfe96caf383d6f3f7c2d94c444a
afu9525 <129424671+afu9525@users.noreply.github.com>
2023-04-01 14:31:05 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const OWNER = " afu9525";
 export const REPO = "ChatGPT";
-export const REPO_URL = `https://qcu.quschool.top`;
+export const REPO_URL = `https://qm.qq.com/cgi-bin/qm/qr?k=F_qECUWRgjMTEKKDvkIAwlLrKnqXT-I3&noverify=0&personal_qrcode_source=2`;
 export const UPDATE_URL = `https://qcu.quschool.top`;
 export const FETCH_COMMIT_URL = `https://qcu.quschool.top`;
 export const FETCH_TAG_URL = `https://qcu.quschool.top`;
